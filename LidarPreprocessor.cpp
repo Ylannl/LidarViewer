10837ac6abb9def2874d1ed43c38afc1a81974ae
Ylannl <r.y.peters@tudelft.nl>
2016-09-09 11:37:16 +0200

    intensity is read and processed properly now

--- LidarPreprocessor.cpp
+++ LidarPreprocessor.cpp
@@ -200,16 +200,16 @@ void loadPointFileLas(PointAccumulator& pa,const char* fileName,unsigned int cla
 			file->skip<double>(1);
 		
 		PointAccumulator::Color c;
-		if(false)//(pointDataFormat>=2) // -- HACK to force reading intensity
+		if(pointDataFormat>=2)
 			{
 			/* Assign point color from stored RGB data: */
 			unsigned short rgb[3];
 			file->read(rgb,3);
 			for(int j=0;j<3;++j)
 				c[j]=float(rgb[j]);
-			}
-		else
-			{
+		// 	}
+		// else
+		// 	{
 			/* Assign point color from intensity: */
 			for(int j=0;j<3;++j)
 				c[j]=float(intensity);
